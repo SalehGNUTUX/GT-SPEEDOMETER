bfda8000a478941b477cdca98bf3eb759bc3fd59
SalehGNUTUX <gnutux.arabic@gmail.com>
2026-08-25 08:48:50 +0100

    رفع الإصدار التجريبيّ إلى 0.13.0-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "net.gnutux.speedometer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "0.12.0-beta"
+        versionCode = 19
+        versionName = "0.13.0-beta"
 
     }
 
